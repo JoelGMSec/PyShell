a500b250500e8c358408e87499f69289cabd44bf
Joel GM <34335312+JoelGMSec@users.noreply.github.com>
2022-05-17 13:21:12 +0200

    Update pyshell.py

--- pyshell.py
+++ pyshell.py
@@ -167,6 +167,7 @@ try:
                      f = open(localfile, "wb") ; f.write(download) ; f.close()
                else:
                   if "pwd" in command.split()[0]:
+                     pwd = str(pwd) + "\n"
                      print (colored(path, "yellow"))
                   else:
                      if "cd" in command.split()[0]:
